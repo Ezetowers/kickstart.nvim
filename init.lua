320885fadb29b012c83a5b43f5d099685e224cc6
Ezequiel Torres Feyuk <ezequiel.torresfeyuk@gmail.com>
2024-12-08 23:14:20 -0300

    Fixes

--- init.lua
+++ init.lua
@@ -677,8 +677,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-				'autopep8', -- Used to format Python code
-				'flake8', -- Used to format Python code
+        'autopep8', -- Used to format Python code
+        'flake8', -- Used to format Python code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
